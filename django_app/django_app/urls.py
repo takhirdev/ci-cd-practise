29943d3f5c1c3aef5162ace6f31767f74a1b198e
takhir <takhir.ismailoff@gmail.com>
2025-06-09 17:55:40 +0500

    collectstatic

--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -20,3 +20,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG or True:  # DEBUG=False bo‘lsa ham vaqtincha ishlaydi
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
