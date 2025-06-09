36b838c55b91dffae71bcd5faa12973f759ad069
takhir <takhir.ismailoff@gmail.com>
2025-06-09 18:42:14 +0500

    xaxaxa

--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('', home, name='home'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
