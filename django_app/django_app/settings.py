badf7bbdec65f852fe6d7e9f46d324f447a2f682
takhir <takhir.ismailoff@gmail.com>
2025-06-09 17:57:11 +0500

    collectstatic

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -15,12 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # << statik fayllar shu joyga yig‘iladi
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),  # agar siz `static/` papkani ishlatayotgan bo‘lsangiz
-]
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
