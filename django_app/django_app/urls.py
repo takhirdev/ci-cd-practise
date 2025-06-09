892e2bd6ee6f177d6c7b5a282227af8d6fec33aa
takhir <takhir.ismailoff@gmail.com>
2025-06-09 18:45:51 +0500

    fixed

--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django_app.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
